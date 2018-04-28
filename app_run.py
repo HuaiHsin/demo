411bf2f401840793afbafbbdb5a69c40eef3dfec
HuaiHsin <huaihsin@gmail.com>
2018-04-28 11:31:44 +0800

    rewrite

--- app_run.py
+++ app_run.py
@@ -1,1 +1,14 @@
-print("hello world!")
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/<username>')
+def show_hello(username):
+    return 'Hello {}'.format(username)
+
+if __name__ == '__main__':
+app.run(debug=True)
